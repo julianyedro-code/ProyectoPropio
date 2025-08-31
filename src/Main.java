d6767c44bd3e476558ac0277b88fbe7a1d7c6906
julianyedro-code <julian.yedro@davinci.edu.ar>
2025-08-31 18:17:45 -0300

    v2
    Cambio del inicio

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import javax.swing.JOptionPane;
 
 public class Main {
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(null, "INICIO");
+		JOptionPane.showMessageDialog(null, "Inicio del programa");
 	}
 }
